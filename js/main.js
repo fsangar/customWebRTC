78b89c652c24d676dd08f093109b9e6ec0779d4a
fsangar <57873286+fsangar@users.noreply.github.com>
2023-02-24 14:02:36 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -62,94 +62,93 @@ let localStream;
 let localStreamElement = document.querySelector('#localStream');
 let remoteStreamElement = document.querySelector('#remoteStream');
 
-function establecerLlamada(){
-    /* Obtiene los datos de audio y video, si todo va bien se conecta al websocket */
-    let getLocalStream = () => {
-        navigator.mediaDevices.getUserMedia(audioVideo)
-            .then((stream) => {
-                console.log('Stream found');
-                localStream = stream;
-                localStreamElement.srcObject = stream;
-                // Connect after making sure that local stream is availble
-                socket.connect();
-            })
-            .catch(error => {
-                console.error('Stream not found: ', error);
-            });
-    }
-
-    /* Creamos una conexión P2P */
-    let createPeerConnection = () => {
-        try {
-            pc = new RTCPeerConnection(PC_CONFIG);
-            pc.onicecandidate = onIceCandidate;
-            pc.onaddstream = onAddStream;
-            pc.addStream(localStream);
-            console.log('PeerConnection created');
-        } catch (error) {
-            console.error('PeerConnection failed: ', error);
-        }
-    };
-
-    /** Recibimos respuestas y enviamos información por P2P*/
-    let sendOffer = () => {
-        console.log('Send offer');
-        pc.createOffer().then(
-            setAndSendLocalDescription,
-            (error) => { console.error('Send offer failed: ', error); }
-        );
-    };
-
-    let sendAnswer = () => {
-        console.log('Send answer');
-        pc.createAnswer().then(
-            setAndSendLocalDescription,
-            (error) => { console.error('Send answer failed: ', error); }
-        );
-    };
-
-    let setAndSendLocalDescription = (sessionDescription) => {
-        pc.setLocalDescription(sessionDescription);
-        console.log('Local description set');
-        sendData(sessionDescription);
-    };
-
-    let onIceCandidate = (event) => {
-        if (event.candidate) {
-            console.log('ICE candidate');
-            sendData({
-                type: 'candidate',
-                candidate: event.candidate
-            });
-        }
-    };
-
-    let onAddStream = (event) => {
-        console.log('Add stream');
-        remoteStreamElement.srcObject = event.stream;
-    };
-
-    let handleSignalingData = (data) => {
-        switch (data.type) {
-            case 'offer':
-                createPeerConnection();
-                pc.setRemoteDescription(new RTCSessionDescription(data));
-                sendAnswer();
-                break;
-            case 'answer':
-                pc.setRemoteDescription(new RTCSessionDescription(data));
-                break;
-            case 'candidate':
-                pc.addIceCandidate(new RTCIceCandidate(data.candidate));
-                break;
-        }
-    }
-    // Comenzamos la conexión
-    getLocalStream();
+/* Obtiene los datos de audio y video, si todo va bien se conecta al websocket */
+let getLocalStream = () => {
+    navigator.mediaDevices.getUserMedia(audioVideo)
+        .then((stream) => {
+            console.log('Stream found');
+            localStream = stream;
+            localStreamElement.srcObject = stream;
+            // Connect after making sure that local stream is availble
+            socket.connect();
+        })
+        .catch(error => {
+            console.error('Stream not found: ', error);
+        });
 }
 
+/* Creamos una conexión P2P */
+let createPeerConnection = () => {
+    try {
+        pc = new RTCPeerConnection(PC_CONFIG);
+        pc.onicecandidate = onIceCandidate;
+        pc.onaddstream = onAddStream;
+        pc.addStream(localStream);
+        console.log('PeerConnection created');
+    } catch (error) {
+        console.error('PeerConnection failed: ', error);
+    }
+};
 
-joinRoom.addEventListener("click", establecerLlamada);
+/** Recibimos respuestas y enviamos información por P2P*/
+let sendOffer = () => {
+    console.log('Send offer');
+    pc.createOffer().then(
+        setAndSendLocalDescription,
+        (error) => { console.error('Send offer failed: ', error); }
+    );
+};
+
+let sendAnswer = () => {
+    console.log('Send answer');
+    pc.createAnswer().then(
+        setAndSendLocalDescription,
+        (error) => { console.error('Send answer failed: ', error); }
+    );
+};
+
+let setAndSendLocalDescription = (sessionDescription) => {
+    pc.setLocalDescription(sessionDescription);
+    console.log('Local description set');
+    sendData(sessionDescription);
+};
+
+let onIceCandidate = (event) => {
+    if (event.candidate) {
+        console.log('ICE candidate');
+        sendData({
+            type: 'candidate',
+            candidate: event.candidate
+        });
+    }
+};
+
+let onAddStream = (event) => {
+    console.log('Add stream');
+    remoteStreamElement.srcObject = event.stream;
+};
+
+let handleSignalingData = (data) => {
+    switch (data.type) {
+        case 'offer':
+            createPeerConnection();
+            pc.setRemoteDescription(new RTCSessionDescription(data));
+            sendAnswer();
+            break;
+        case 'answer':
+            pc.setRemoteDescription(new RTCSessionDescription(data));
+            break;
+        case 'candidate':
+            pc.addIceCandidate(new RTCIceCandidate(data.candidate));
+            break;
+    }
+}
+// Comenzamos la conexión
+/*getLocalStream();*/
+
+
+
+joinRoom.addEventListener("click", getLocalStream);
 
 
 
