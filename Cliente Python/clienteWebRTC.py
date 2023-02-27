4c0e50046f5ef19c0f335b81cf8eacad39f4b0b2
fsangar <57873286+fsangar@users.noreply.github.com>
2023-02-27 14:30:54 +0100

    Update clienteWebRTC.py

--- Cliente Python/clienteWebRTC.py
+++ Cliente Python/clienteWebRTC.py
@@ -2,6 +2,7 @@ import asyncio
 from aiortc import RTCPeerConnection, RTCSessionDescription
 import websockets
 import ssl
+import pathlib
 
 # URLS
 urlWS = 'wss://api-rest-teleasistencia-p1.iesvjp.es:9999'
