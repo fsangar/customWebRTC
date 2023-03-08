a17e50ce7d5a7d5f97f63388c8a3cbb4bd7be644
fsangar <57873286+fsangar@users.noreply.github.com>
2023-03-08 13:53:29 +0100

    Update client.py

--- Cliente Python/withSocketIO/client.py
+++ Cliente Python/withSocketIO/client.py
@@ -25,54 +25,54 @@ async def setParamsConnection():
     http_session = aiohttp.ClientSession(connector=connector)
     sio = socketio.AsyncClient(http_session=http_session)
 
-    @sio.event
-    def message(data):
-        print('I received a message!')
+@sio.event
+def message(data):
+    print('I received a message!')
 
-    @sio.on('my message')
-    async def on_message(data):
-        print('I received a message!')
-        # Esperar la respuesta del servidor WebSocket
-        response = data
-        answer = {'type': RTCSessionDescription.ANSWER, 'sdp': response}
-        # Configurar el objeto RTCSessionDescription remoto
-        await pc.setRemoteDescription(RTCSessionDescription(answer['sdp'], answer['type']))
-        # Agregar un evento de escucha para la pista
-        pc.on("track", on_track)
-        # Esperar a que se cierre la conexión WebRTC
-        await pc.wait_closed()
+@sio.on('my message')
+async def on_message(data):
+    print('I received a message!')
+    # Esperar la respuesta del servidor WebSocket
+    response = data
+    answer = {'type': RTCSessionDescription.ANSWER, 'sdp': response}
+    # Configurar el objeto RTCSessionDescription remoto
+    await pc.setRemoteDescription(RTCSessionDescription(answer['sdp'], answer['type']))
+    # Agregar un evento de escucha para la pista
+    pc.on("track", on_track)
+    # Esperar a que se cierre la conexión WebRTC
+    await pc.wait_closed()
 
-    @sio.event
-    async def message(data):
-        print('I received a message!')
+@sio.event
+async def message(data):
+    print('I received a message!')
 
-    @sio.on('*')
-    def catch_all(event, data):
-        pass
+@sio.on('*')
+def catch_all(event, data):
+    pass
 
-    @sio.on('*')
-    async def catch_all(event, data):
-       pass
+@sio.on('*')
+async def catch_all(event, data):
+   pass
 
-    @sio.event
-    async def connect():
-         print("---  Conectado al WebSocket  --- ")
-         # Crear un objeto RTCPeerConnection
-         pc = RTCPeerConnection()
-         pc.addIceServer(turn_server)
-         # Crear y configurar el objeto RTCSessionDescription local
-         await pc.setLocalDescription(await pc.createOffer())
-         offer = {'type': pc.localDescription.type, 'sdp': pc.localDescription.sdp}
-         # Enviar la oferta al servidor WebSocket
-         await sio.emit(offer)
+@sio.event
+async def connect():
+     print("---  Conectado al WebSocket  --- ")
+     # Crear un objeto RTCPeerConnection
+     pc = RTCPeerConnection()
+     pc.addIceServer(turn_server)
+     # Crear y configurar el objeto RTCSessionDescription local
+     await pc.setLocalDescription(await pc.createOffer())
+     offer = {'type': pc.localDescription.type, 'sdp': pc.localDescription.sdp}
+     # Enviar la oferta al servidor WebSocket
+     await sio.emit(offer)
 
-    @sio.event
-    def connect_error(data):
-        print("The connection failed!")
+@sio.event
+def connect_error(data):
+    print("The connection failed!")
 
-    @sio.event
-    def disconnect():
-        print("I'm disconnected!")
+@sio.event
+def disconnect():
+    print("I'm disconnected!")
 
 
 # Escuchar los eventos de la conexión WebRTC
