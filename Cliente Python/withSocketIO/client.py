f43fe7d34134691b31d9ef740838d794491aed63
fsangar <57873286+fsangar@users.noreply.github.com>
2023-03-08 13:52:54 +0100

    Update client.py

--- Cliente Python/withSocketIO/client.py
+++ Cliente Python/withSocketIO/client.py
@@ -54,25 +54,25 @@ async def setParamsConnection():
     async def catch_all(event, data):
        pass
 
-@sio.event
-async def connect():
-     print("---  Conectado al WebSocket  --- ")
-     # Crear un objeto RTCPeerConnection
-     pc = RTCPeerConnection()
-     pc.addIceServer(turn_server)
-     # Crear y configurar el objeto RTCSessionDescription local
-     await pc.setLocalDescription(await pc.createOffer())
-     offer = {'type': pc.localDescription.type, 'sdp': pc.localDescription.sdp}
-     # Enviar la oferta al servidor WebSocket
-     await sio.emit(offer)
+    @sio.event
+    async def connect():
+         print("---  Conectado al WebSocket  --- ")
+         # Crear un objeto RTCPeerConnection
+         pc = RTCPeerConnection()
+         pc.addIceServer(turn_server)
+         # Crear y configurar el objeto RTCSessionDescription local
+         await pc.setLocalDescription(await pc.createOffer())
+         offer = {'type': pc.localDescription.type, 'sdp': pc.localDescription.sdp}
+         # Enviar la oferta al servidor WebSocket
+         await sio.emit(offer)
 
-@sio.event
-def connect_error(data):
-    print("The connection failed!")
+    @sio.event
+    def connect_error(data):
+        print("The connection failed!")
 
-@sio.event
-def disconnect():
-    print("I'm disconnected!")
+    @sio.event
+    def disconnect():
+        print("I'm disconnected!")
 
 
 # Escuchar los eventos de la conexión WebRTC
